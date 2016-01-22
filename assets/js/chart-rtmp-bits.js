a76d4d7a92e0259d3906d1b8a5db1f651a988b67
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-01-21 23:09:38 -0200

    update assets

---
+++ assets/js/chart-rtmp-bits.js
@@ -0,0 +1,71 @@
+;(function (window, document, undefined) {
+
+  'use strict';
+
+  var ChartRtmpBits = (function () {
+
+    var data = [
+      {
+        key: 'In',
+        color: '#7777ff',
+        values: []
+      },
+      {
+        key: 'Out',
+        color: '#DE4545',
+        values: []
+      }
+    ];
+
+    var chart;
+
+    var _populate = function (bindto) {
+      d3.select(bindto)
+        .datum(data)
+        .call(chart);
+    }
+
+    return {
+
+      init : function (bindto, timestamp, bw_in, bw_out) {
+        data[0].values.push({ x: timestamp, y: bw_in });
+        data[1].values.push({ x: timestamp, y: bw_out });
+
+        if (chart) {
+          _populate(bindto);
+          chart.update();
+        } else {
+          nv.addGraph(function() {
+            chart = nv.models.lineChart().options({
+              duration: 0,
+              useInteractiveGuideline: true,
+              interactive: false,
+              showLegend: true,
+              showXAxis: true,
+              showYAxis: true
+            });
+
+            chart.xAxis
+              .axisLabel('Uptime (ms)')
+              .tickFormat(d3.format(',r'));
+
+            chart.yAxis
+              .axisLabel('Bits')
+              .tickFormat(d3.format('d'));
+
+            _populate(bindto);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+          });
+        }
+      }
+
+    }
+
+  })();
+
+  window.ChartRtmpBits = ChartRtmpBits;
+
+})(window, document);
